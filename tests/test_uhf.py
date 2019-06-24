e4aade9c7c7d93da3c983c6e72ff3a7ae86d458d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-06-24 17:13:39 +0200

    Add UHF-test using PySCF

--- tests/test_uhf.py
+++ tests/test_uhf.py
@@ -1,10 +1,21 @@
 import psi4
+import pyscf
 from hartree_fock import UHF
 from hartree_fock.mix import DIIS, AlphaMixer
 from quantum_systems import (
     TwoDimensionalHarmonicOscillator,
     construct_psi4_system,
+    construct_pyscf_system_ao,
 )
+
+
+def get_pysf_uhf_energy(molecule, basis="ccpvdz", **kwargs):
+    mol = pyscf.gto.Mole()
+    mol.build(atom=molecule, basis=basis, **kwargs)
+
+    uhf = pyscf.scf.UHF(mol)
+
+    return uhf.kernel() + mol.energy_nuc()
 
 
 def get_psi4_uhf_energy(molecule, options):
@@ -68,3 +79,18 @@ def test_triplet_O2_uhf():
     uhf.compute_ground_state(tol=1e-8)
 
     assert abs(uhf.compute_energy() - psi4_energy) < 1e-3
+
+
+def test_lithium_uhf():
+    mol = "li"
+    basis = "ccpvdz"
+    kwargs = dict(spin=1)
+
+    pyscf_energy = get_pysf_uhf_energy(mol, basis, **kwargs)
+    system = construct_pyscf_system_ao(
+        mol, basis, add_spin=False, anti_symmetrize=False, **kwargs
+    )
+    uhf = UHF(system, verbose=True)
+    uhf.compute_ground_state(tol=1e-8)
+
+    assert abs(uhf.compute_energy() - pyscf_energy) < 1e-6
