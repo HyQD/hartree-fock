3145926a995d038f42648347762f2f56cb05cd9f
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-11-22 13:54:25 +0100

    Add test of density matrix

---
+++ tests/test_helper.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from hartree_fock.hf_helper import build_density_matrix
+
+
+def test_density_matrix():
+    n = 4
+    l = 20
+
+    o = slice(0, n)
+
+    for i in range(10):
+        C = np.random.random((l, l)) + 1j * np.random.random((l, l))
+
+        D = np.einsum("pi, qi -> pq", C[:, o].conj(), C[:, o])
+        D_helper = build_density_matrix(C, o, np)
+
+        np.testing.assert_allclose(D, D_helper)
