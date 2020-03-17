ecf07e2142fc9181a94f9d87f7c2d2c5499aa393
haakoek <haakoek@student.matnat.uio.no>
2020-03-17 21:13:03 +0100

    Use h20 sto-3g as test case for rhf and ghf

---
+++ tests/test_ghf.py
@@ -0,0 +1,28 @@
+from hartree_fock.ghf import GHF
+from quantum_systems import TwoDimensionalHarmonicOscillator
+from quantum_systems import construct_pyscf_system_ao
+import numpy as np
+
+def test_ghf():
+    
+    r=1.871
+    molecule = f'O; H, 1, {r};  H 1 {r} 2 100.0'
+    basis = 'sto-3g'
+    
+    system = construct_pyscf_system_ao(
+        molecule,
+        basis=basis,
+        np=np,
+        verbose=False,
+        add_spin=True,
+        anti_symmetrize=True,
+    )
+    
+    ghf = GHF(system,verbose=True)
+    ghf.compute_ground_state(tol=1e-10)
+    e_hf_pyscf = -74.9659001731752
+    assert abs(rhf.energy()-e_hf_pyscf) < 1e-10
+    
+
+if __name__=='__main__':
+    test_ghf()
