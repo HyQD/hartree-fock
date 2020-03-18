a17b613e57d9dd598751d78d8e9f92e3f70a0338
haakoek <haakoek@student.matnat.uio.no>
2020-03-18 13:35:56 +0100

    remove redundant constructor

--- hartree_fock/ghf/ghf.py
+++ hartree_fock/ghf/ghf.py
@@ -2,9 +2,6 @@ from hartree_fock.hf import HartreeFock
 
 
 class GHF(HartreeFock):
-    def __init__(self, system, **kwargs):
-        super().__init__(system, **kwargs)
-
     def build_density_matrix(self, C):
         P = self.np.einsum(
             "ui,vi->uv", C[:, self.system.o], C[:, self.system.o].conj()
