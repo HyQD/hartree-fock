d884ab5cf4075b3af504d7b2f56921cf2e503868
haakoek <haakoek@student.matnat.uio.no>
2020-03-18 01:38:06 +0100

    test alphamixer and diis for rhf

--- tests/test_rhf.py
+++ tests/test_rhf.py
@@ -2,6 +2,7 @@ from hartree_fock.rhf import RHF
 from quantum_systems import TwoDimensionalHarmonicOscillator
 from quantum_systems import construct_pyscf_system_ao
 import numpy as np
+from hartree_fock.mix import AlphaMixer, DIIS
 
 
 def test_rhf():
@@ -9,7 +10,8 @@ def test_rhf():
     r = 1.871
     molecule = f"O; H, 1, {r};  H 1 {r} 2 100.0"
     basis = "sto-3g"
-
+    e_hf_pyscf = -74.965_900_173_175_2
+    
     system = construct_pyscf_system_ao(
         molecule,
         basis=basis,
@@ -19,10 +21,14 @@ def test_rhf():
         anti_symmetrize=False,
     )
 
-    rhf = RHF(system, verbose=True)
-    rhf.compute_ground_state(tol=1e-10)
-    e_hf_pyscf = -74.965_900_173_175_2
-    assert abs(rhf.total_energy - e_hf_pyscf) < 1e-10
+    rhf_diis = RHF(system,mixer=DIIS, verbose=False)
+    rhf_diis.compute_ground_state(tol=1e-10)
+    assert abs(rhf_diis.total_energy - e_hf_pyscf) < 1e-10
+
+    rhf_alpha = RHF(system,mixer=AlphaMixer, verbose=False)
+    mixer_kwargs=dict(theta=0.2)
+    rhf_alpha.compute_ground_state(tol=1e-10,**mixer_kwargs)
+    assert abs(rhf_alpha.total_energy - e_hf_pyscf) < 1e-10
 
 
 def test_tdho_rhf():
