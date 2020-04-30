ea0d64dc0a990e3a9256b1f53398449b9e750b48
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-04-30 14:09:07 +0200

    Move dependencies to setup

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name="Hartree-Fock", version="0.0.1", packages=find_packages())
+setup(
+    name="Hartree-Fock",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "quantum-systems @ git+https://github.com/Schoyen/quantum-systems",
+    ],
+)
