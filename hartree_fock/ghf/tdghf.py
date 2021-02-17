25bc32c7202f9f22dc104fafc8751133298a6453
haakoek <haakoek@student.matnat.uio.no>
2021-02-17 13:20:30 +0100

    setup tdghf and fill in energy, density_matrix, fock_matrix and one_body_expectation_value functions.

--- hartree_fock/ghf/tdghf.py
+++ hartree_fock/ghf/tdghf.py
@@ -5,6 +5,8 @@ class TDGHF(TimeDependentHartreeFock):
     def compute_energy(self, current_time, C):
         np = self.np
 
+        self.update_hamiltonian(current_time)
+
         C = C.reshape(self.system.l, self.system.l)
         density_matrix = self.build_density_matrix(C, self.o)
 
@@ -17,3 +19,28 @@ class TDGHF(TimeDependentHartreeFock):
         energy += np.trace(np.dot(density_matrix, term))
 
         return energy + self.system.nuclear_repulsion_energy
+
+    def compute_one_body_expectation_value(self, current_time, C, mat):
+        D = self.build_density_matrix(C, self.system.o)
+        return self.np.trace(self.np.dot(D, mat))
+
+    def compute_one_body_density_matrix(self, current_time, C):
+        pass
+
+    def compute_particle_density(self, current_time, C):
+        pass
+
+    def compute_overlap(self, current_time, C_a, C_b):
+        pass
+
+    def build_density_matrix(self, C):
+        D = self.np.einsum(
+            "ui,vi->uv", C[:, self.system.o], C[:, self.system.o].conj()
+        )
+        return D
+
+    def build_fock_matrix(self, h, u, density_matrix):
+        F = self.h + self.np.einsum(
+            "ls,usvl->uv", density_matrix, self.system.u
+        )
+        return F
