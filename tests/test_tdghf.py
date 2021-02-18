890efb0c79c84f4e6c7b58ebab75bd96bc813ab7
haakoek <haakoek@student.matnat.uio.no>
2021-02-18 10:38:50 +0100

    Reproduce H2 dipole moment from Li et al. paper

--- tests/test_tdghf.py
+++ tests/test_tdghf.py
@@ -13,6 +13,112 @@ from quantum_systems import construct_pyscf_system_ao
 
 from matplotlib import pyplot as plt
 
+@pytest.mark.skip
+def test_h2():
+
+    """
+    This setup reproduces figure 4 in the paper:
+    Li, Xiaosong, et al. "A time-dependent Hartree–Fock approach for
+    studying the electronic optical response of molecules in intense fields."
+    Physical Chemistry Chemical Physics 7.2 (2005): 233-239.
+
+    The computation is a bit to lengthy to be used as a test though.
+    """
+
+    class linear_laser:
+        def __init__(self, E0, omega):
+            self.E0 = E0
+            self.omega = omega
+            self.tc = 2 * np.pi / omega
+
+        def __call__(self, t):
+            if 0 <= t <= self.tc:
+                return t / self.tc * self.E0 * np.sin(self.omega * t)
+            elif self.tc <= t <= 2 * self.tc:
+                return self.E0 * np.sin(self.omega * t)
+            elif 2 * self.tc <= t <= 3 * self.tc:
+                return (3 - t / self.tc) * self.E0 * np.sin(self.omega * t)
+            else:
+                return 0
+
+    molecule = "h 0.0 0.0 -0.69485; h 0.0 0.0 0.69485"
+
+    basis = "6-311++Gss"
+
+    system = construct_pyscf_system_ao(
+        molecule,
+        basis=basis,
+        np=np,
+        verbose=False,
+        add_spin=True,
+        anti_symmetrize=True,
+    )
+
+    ghf = GHF(system, verbose=False)
+    ghf.compute_ground_state(tol=1e-12, change_system_basis=True)
+
+    laser_pulse = linear_laser(E0=0.07, omega=0.1)
+    polarization = np.zeros(3)
+    polarization_direction = 2
+    polarization[polarization_direction] = 1
+    system.set_time_evolution_operator(
+        DipoleFieldInteraction(laser_pulse, polarization_vector=polarization)
+    )
+
+    tdghf = TDGHF(system, verbose=True)
+    r = complex_ode(tdghf).set_integrator("GaussIntegrator", s=3, eps=1e-10)
+    r.set_initial_value(ghf.C.ravel())
+
+    dt = 1e-1
+    tfinal = 225
+    num_steps = int(tfinal / dt) + 1
+    print(f"num_steps={num_steps}")
+    time_points = np.linspace(0, tfinal, num_steps)
+
+    energy = np.zeros(num_steps, dtype=np.complex128)
+    overlap = np.zeros(num_steps)
+    dipole_moment = np.zeros((num_steps, 3), dtype=np.complex128)
+
+    energy[0] = tdghf.compute_energy(0, r.y.reshape(system.l, system.l))
+    overlap[0] = tdghf.compute_overlap(
+        0, r.y.reshape(system.l, system.l), ghf.C
+    )
+
+    for j in range(3):
+        dipole_moment[0, j] = -tdghf.compute_one_body_expectation_value(
+            0, r.y.reshape(system.l, system.l), system.position[j]
+        )
+
+    for i in range(num_steps - 1):
+
+        r.integrate(r.t + dt)
+
+        energy[i + 1] = tdghf.compute_energy(
+            r.t + dt, r.y.reshape(system.l, system.l)
+        )
+
+        overlap[i + 1] = tdghf.compute_overlap(
+            r.t + dt, r.y.reshape(system.l, system.l), ghf.C
+        )
+
+        for j in range(3):
+            dipole_moment[i + 1, j] = -tdghf.compute_one_body_expectation_value(
+                r.t + dt, r.y.reshape(system.l, system.l), system.position[j]
+            )
+        if i % 100 == 0:
+            print(i)
+
+    plt.figure()
+    plt.plot(time_points, energy.real)
+
+    plt.figure()
+    plt.plot(time_points, overlap)
+
+    plt.figure()
+    plt.plot(time_points, dipole_moment[:, polarization_direction].real)
+
+    plt.show()
+
 
 @pytest.mark.skip
 def test_helium():
@@ -198,4 +304,4 @@ def test_tdhf():
 
 
 if __name__ == "__main__":
-    test_tdhf()
+    test_h2()
